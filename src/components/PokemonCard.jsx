eb0909427bacf33c776a36d992119faeb49ea915
TZARD69 <zair.hamza69@gmail.com>
2023-03-29 21:44:11 +0200

    second commit exercise PokemonCard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,25 @@
 function PokemonCard(){
-    return (
-    <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-         alt="image Pokemone bulbasaur"/>
-        <figcaption>bulbasaur</figcaption>
-    </figure>
-)
+  
+   const pokemon = pokemonList[0]
 
+   return(
+  
+    <figure className="card">
+       {pokemon.imgSrc? <img src={pokemon.imgSrc} alt="image Pokemone bulbasaur" className="card-img"/> : <p>???</p>}       
+        <figcaption>{pokemon.name}</figcaption>
+    </figure>)
+   
 }
-export default PokemonCard;
+
+
+const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  export default PokemonCard;
